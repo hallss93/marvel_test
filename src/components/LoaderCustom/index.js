c029678a769d2dc71782cd984d2d8d8ab2ae9925
Hallison Melo <hallison.melo@mesainc.com.br>
2021-03-08 11:21:19 -0300

    fix files don't include git

--- src/components/LoaderCustom/index.js
+++ src/components/LoaderCustom/index.js
@@ -5,24 +5,24 @@ const animationData = require("../../assets/lottie/loader.json");
 const animationDataSpin = require("../../assets/lottie/spin.json");
 
 const Loader = ({ height = 350, width = 350, spin = false }) => {
-  const animationOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: spin ? animationDataSpin : animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  return (
-    <Container>
-      <Lottie
-        style={{ pointerEvents: "none" }}
-        options={animationOptions}
-        height={height}
-        width={width}
-      />
-    </Container>
-  );
+    const animationOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: spin ? animationDataSpin : animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    };
+    return (
+        <Container>
+            <Lottie
+                style={{ pointerEvents: "none" }}
+                options={animationOptions}
+                height={height}
+                width={width}
+            />
+        </Container>
+    );
 };
 
 export default Loader;
---
+++ src/store/actionsType.js
@@ -0,0 +1,12 @@
+const actions = {
+    character: {
+        fetch: "@character/FETCH",
+        fetchSuccess: "@character/FETCH_SUCCESS",
+        fetchFailure: "@character/FETCH_FAILURE",
+        show: "@character/SHOW",
+        showSuccess: "@character/SHOW_SUCCESS",
+        showFailure: "@character/SHOW_FAILURE",
+    },
+};
+
+export default actions;
