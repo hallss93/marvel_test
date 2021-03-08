e6ce15dccc8695cc1aaba9b81aea0221ec4ad273
Hallison Melo <hallison.melo@mesainc.com.br>
2021-03-08 09:49:15 -0300

    fix eslint

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,69 +1,81 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, CardView, CardDiv, ImageContainer, Image, Overlay } from "./styles";
-import Select from 'react-select'
-import LoaderCustom from '../../components/LoaderCustom';
+import {
+  Container,
+  CardView,
+  CardDiv,
+  ImageContainer,
+  Image,
+  Overlay,
+} from "./styles";
+import Select from "react-select";
+import LoaderCustom from "../../components/LoaderCustom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetch } from "../../store/actions/characterAction";
-import Play from "../../assets/img/play.png"
+import Play from "../../assets/img/play.png";
 
 const orderByOptions = [
   {
-    label: 'Nome crescente',
-    value: 'name'
+    label: "Nome crescente",
+    value: "name",
   },
   {
-    label: 'Nome decrescente',
-    value: '-name'
+    label: "Nome decrescente",
+    value: "-name",
   },
-]
+];
 
 const Home = () => {
   const dispatch = useDispatch();
   const navigation = useNavigate();
   const limit = 30;
-  const [series, setSeries] = useState([])
-  const [page, setPage] = useState(1)
-  const [orderBy, setOrderBy] = useState(orderByOptions[1])
-  const [hasMore, setHasMore] = useState(true)
-  const [isLoading, setIsLoading] = useState(false)
+  const [series, setSeries] = useState([]);
+  const [page, setPage] = useState(1);
+  const [orderBy, setOrderBy] = useState(orderByOptions[1]);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchChars = useSelector(({ character }) => character.characters);
 
   useEffect(() => {
     window.onscroll = function (ev) {
-      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
         if (hasMore && !isLoading) {
-          setPage(prev => prev + 1)
-          setIsLoading(true)
+          setPage((prev) => prev + 1);
+          setIsLoading(true);
         }
       }
     };
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (fetchChars !== null) {
-      if (fetchChars.length < limit) setHasMore(false)
+      if (fetchChars.length < limit) setHasMore(false);
       setSeries([...series, ...fetchChars]);
     }
   }, [fetchChars]);
 
   useEffect(() => {
     dispatch(fetch({ page, orderBy: orderBy.value, limit }));
-  }, [page, orderBy])
-
+  }, [page, orderBy]);
 
   const CardItem = ({ data, index }) => {
     const handleClick = () => {
       navigation(`/detalhes/${data.id}`);
     };
     let url = `${data.thumbnail.path}.${data.thumbnail.extension}`;
-    let haveImage = url !== 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+    let haveImage =
+      url !==
+      "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg";
     return (
-      <CardDiv className={page == 1 && `animated fadeIn delay-${index > 10 ? 10 : index
-        }00ms`} onClick={handleClick}>
+      <CardDiv
+        className={
+          page === 1 && `animated fadeIn delay-${index > 10 ? 10 : index}00ms`
+        }
+        onClick={handleClick}
+      >
         <ImageContainer>
-          <Image position={haveImage ? 'center' : 'left'} url={url} />
+          <Image position={haveImage ? "center" : "left"} url={url} />
           <Overlay>
             <img src={Play} alt="..." />
           </Overlay>
@@ -78,10 +90,12 @@ const Home = () => {
 
   if (series.length <= 0) {
     return (
-      <div style={{ minHeight: 300, display: 'flex', justifyContent: 'center' }} >
+      <div
+        style={{ minHeight: 300, display: "flex", justifyContent: "center" }}
+      >
         <LoaderCustom width={150} height={250} />
-      </div >
-    )
+      </div>
+    );
   }
   return (
     <Container>
@@ -89,19 +103,19 @@ const Home = () => {
         styles={{
           control: (base) => ({
             ...base,
-            height: '40px',
+            height: "40px",
             width: 300,
-            border: '1px solid #0000001F',
-            borderRadius: ' 4px',
+            border: "1px solid #0000001F",
+            borderRadius: " 4px",
             margin: 25,
           }),
         }}
         options={orderByOptions}
         value={orderBy}
-        onChange={value => {
-          setSeries([])
-          setPage(1)
-          setOrderBy(value)
+        onChange={(value) => {
+          setSeries([]);
+          setPage(1);
+          setOrderBy(value);
         }}
         placeholder="Ordenar por"
       />
@@ -113,7 +127,9 @@ const Home = () => {
         </CardView>
       )}
       {isLoading && (
-        <div style={{ minHeight: 300, display: 'flex', justifyContent: 'center' }} >
+        <div
+          style={{ minHeight: 300, display: "flex", justifyContent: "center" }}
+        >
           <LoaderCustom spin width={100} height={100} />
         </div>
       )}
